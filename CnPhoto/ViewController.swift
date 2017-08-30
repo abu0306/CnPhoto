5a6fbecb3af66134fb3fe65840aa0cc48b963625
abu <2261983469@qq.com>
2017-08-30 17:36:07 +0800

    完善demo

--- CnPhoto/ViewController.swift
+++ CnPhoto/ViewController.swift
@@ -16,11 +16,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let btn = UIButton(type: .custom)
-        btn.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
-        btn.backgroundColor = UIColor.yellow
-        btn.addTarget(self, action: #selector(btnAction), for: .touchUpInside)
-        view.addSubview(btn)
+        let btn_single = UIButton(type: .custom)
+        btn_single.frame = CGRect(x: 50, y: 100, width: 100, height: 50)
+        btn_single.backgroundColor = UIColor.yellow
+        btn_single.setTitle("单选照片", for: .normal)
+        btn_single.setTitleColor(UIColor.black, for: .normal)
+        btn_single.addTarget(self, action: #selector(singleBtnAction1), for: .touchUpInside)
+        view.addSubview(btn_single)
+        
+        let btn_double = UIButton(type: .custom)
+        btn_double.frame = CGRect(x: cnScreenW - 150, y: 100, width: 100, height: 50)
+        btn_double.backgroundColor = UIColor.yellow
+        btn_double.setTitle("多选照片", for: .normal)
+        btn_double.setTitleColor(UIColor.black, for: .normal)
+        btn_double.addTarget(self, action: #selector(doubleBtnAction2), for: .touchUpInside)
+        view.addSubview(btn_double)
         
         myImageView.frame = CGRect(x: 0, y: 250, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.width)
         myImageView.backgroundColor = UIColor.green
@@ -28,6 +38,9 @@ class ViewController: UIViewController {
         myImageView.contentMode = .scaleAspectFill
         myImageView.clipsToBounds = true
     }
+    
+    
+    /******************单选回调*********************/
     
 //    /// UIImage异步回调
 //    ///
@@ -48,6 +61,7 @@ class ViewController: UIViewController {
     }
     
     
+    /******************多选回调*********************/
     /// [UIImage]多选相册自定义导航异步回调
     ///
     /// - Parameter imgArray: _
@@ -70,19 +84,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    func btnAction() {
+    /// 单选事件
+    func singleBtnAction1() {
         
         /// 单选相册默认导航
         //self.photoAlbum(false)
         
         //单选相册自定义导航
-//        self.photoAlbum(false, .lightContent, UIColor.black, tintColor: UIColor.white, bgColor: UIColor.white)
-        
-        /// 单选相册默认导航
+        self.photoAlbum(false, .lightContent, UIColor.black, tintColor: UIColor.white, bgColor: UIColor.white)
+    }
+    
+    /// 多选事件
+    func doubleBtnAction2()  {
+        /// 多选相册默认导航
         //self.photoAlbum(true)
         
         //多选相册自定义导航
         self.photoAlbum(true, .lightContent, UIColor.black, tintColor: UIColor.white, bgColor: UIColor.white)
+        
     }
 
 }
